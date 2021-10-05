60fd6b37f8c6a5174f17829116d37d51dce8dd48
natchaws <natchaa62@nu.ac.th>
2021-10-06 00:08:18 +0700

    delete

--- server/src/models/Comment.js
+++
@@ -1,9 +0,0 @@
-module.exports = ( sequelize, DataTypes ) => {
-    const Comment = sequelize.define('Comment', {
-      title: DataTypes.STRING,
-      thumbnail: DataTypes.STRING,
-      pictures: DataTypes.STRING,
-      content: DataTypes.TEXT
-    })
-    return Comment
-  }
--- server/src/models/Room.js
+++
@@ -1,9 +0,0 @@
-﻿module.exports = (sequelize, DataTypes) => {
-    const Room = sequelize.define('Room', {
-      nameroom: DataTypes.STRING,
-      peopleroom: DataTypes.STRING
-      
-      
-    })
-    return Room
-}
